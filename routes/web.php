cb60a5899c859e6d132473c4d09f9da50c37b84f
Muhindo Mubaraka <mubs0x@gmail.com>
2023-09-16 02:12:15 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
-    die("r");
+    die("r");//new data
 })->name("gen"); */
 Route::get('/gen', function () {
     die(Gen::find($_GET['id'])->do_get());
