f11c1e519cb5aba225a83f6035abd7b9c0ecc1ce
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-11-13 12:56:03 +0300

    request_status

--- app/Http/Controllers/ApiResurceController.php
+++ app/Http/Controllers/ApiResurceController.php
@@ -143,6 +143,7 @@ class ApiResurceController extends Controller
         $u->business_email = $request->business_email;
         $u->business_cover_photo = $request->business_cover_photo;
         $u->business_cover_details = $request->business_cover_details;
+        $u->request_status = 'Pending';
         $u->vet_service = $request->vet_service;
         $u->status = '5';
 
