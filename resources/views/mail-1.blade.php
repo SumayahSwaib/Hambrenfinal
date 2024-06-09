74874abe045d7d09e341c0d49b975f688bd4195f
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-06-09 14:43:12 +0300

    AS

--- resources/views/mail-1.blade.php
+++ resources/views/mail-1.blade.php
@@ -9,7 +9,7 @@ if (!isset($body)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HAMBREN</title>
+    <title>{{ env('APP_NAME') }}</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
